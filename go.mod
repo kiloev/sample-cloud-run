8540ba1eec4cf6a11b8a40960a509be2fb16836f
kiloev <freakperzon@yahoo.com>
2021-11-20 13:43:54 +0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kiloev/sample-cloud-run
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world!")
+}
